cf301f4141400c0018a61def0c0c35e3ff70c580
Janice Caldeira <janicecaldeira@gmail.com>
2022-09-19 23:53:20 -0300

    feat: add swagger document

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const routes = require("./src/routes/routes");
+const express = require('express')
+const bodyParser = require('body-parser')
+const routes = require('./src/routes/routes')
+const swaggerUI = require('swagger-ui-express')
+const swaggerFile = require('./swagger_output.json')
 
 require('dotenv').config()
 
-const app = express();
+const app = express()
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(bodyParser.json())
 
-app.use("/api", routes);
+app.use('/api', routes)
 
-app.use(express.static('public'));
+app.use(express.static('public'))
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile))
 
 app.listen(port, () => {
-  console.log(`Server is listening on http://localhost:${port}`);
-});
+  console.log(`Server is listening on http://localhost:${port}`)
+})
