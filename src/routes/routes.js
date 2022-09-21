c0108282bbb06979b4cd15c0cb96ed1214c5df9a
Janice Caldeira <janicecaldeira@gmail.com>
2022-09-21 18:28:47 -0300

    feat: add new routes and fix create database

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,9 +3,12 @@ const routes = express.Router()
 
 const clientsController = require('../controllers/clients.controller')
 const loginController = require('../controllers/login.controller')
+const accountsController = require('../controllers/accounts.controller')
 
 routes.get('/client', clientsController.findAll)
 routes.post('/client', clientsController.create)
 routes.post('/login', loginController.login)
+routes.post('/logout', loginController.logout)
+routes.get('/account', accountsController.findAll)
 
 module.exports = routes
