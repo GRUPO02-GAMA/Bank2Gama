fca06904e3686bf79c9205c4c61f7f20b27722c7
Janice Caldeira <janicecaldeira@gmail.com>
2022-09-23 17:10:28 -0300

    fix: find by email

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -4,7 +4,7 @@ const Client = require('../models/clients.model')
 exports.user = async function user(req, res) {
   const user = decode(req.cookies.auth)
 
-  const client = await Client.findAll({ where: { id: user.id } })
+  const client = await Client.findAll({ where: { email: user.email } })
 
   res.json(client)
 }
